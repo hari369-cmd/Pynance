34362a6324119d9b17d491932b41b200ac7b2aa3
keshav2995 <keshav.singh29@gmail.com>
2021-03-13 05:37:53 +0900

    Debug file changes

--- rsi/indicators.py
+++ rsi/indicators.py
@@ -5,34 +5,38 @@ from get_all_tickers import get_tickers as gt
 from statistics import mean
 import talib
 import matplotlib.pyplot as plt
-
-"""
-Working of RSI
-- Observe the last 14 closing prices of a stock.
-- Determine whether the current time period's closing price is higher or lower than the previous one.
-- Calculate the average gain and loss over the last 14 time period's.
-- Compute the relative strength (RS): (AvgGain/AvgLoss)
-- Compute the relative strength index (RSI): (100–100 / ( 1 + RS))
-
-Working with smoothed RSI
-- https://mrjbq7.github.io/ta-lib/doc_index.html
-- Compute the relative strength (RS): EMA smoothed
-"""
+import seaborn as sns 
 
 
 # Get the path for each stock file in a list
 list_files = (glob.glob("/Users/ks/bot/Pynance/yfinance/stock_data_min/*.csv"))
 
-# Create the dataframe that we will be adding the final analysis of each stock to
-compare_stocks = pd.DataFrame(columns=["Company", "Periods_Observed", "Crosses", "True_Positive", "False_Positive", "True_Negative", "False_Negative", "Sensitivity", 
-"Specificity", "Accuracy", "TPR", "FPR"])
 
-# loop to cycle through the stocks 
-for stock in list_files:
+
+"""-TODO: Trend analysis/ volitility / variance / volume in data is important for Fibonacci """
+
+def indicator_rsi(stock, time_period=14):
+    """
+    Working of RSI
+    - Observe the last 14 closing prices of a stock.
+    - Determine whether the current time period's closing price is higher or lower than the previous one.
+    - Calculate the average gain and loss over the last 14 time period's.
+    - Compute the relative strength (RS): (AvgGain/AvgLoss)
+    - Compute the relative strength index (RSI): (100–100 / ( 1 + RS))
+    - TODO: time_period is variable (Neural network)
+
+    Working with smoothed RSI
+    - https://mrjbq7.github.io/ta-lib/doc_index.html
+    - Compute the relative strength (RS): EMA smoothed
+    """
+
+    # Create the dataframe that we will be adding the final analysis of each stock to
+    rsi_power = pd.DataFrame(columns=["Company", "Periods_Observed", "Crosses", "True_Positive", "False_Positive", "True_Negative", "False_Negative", "Sensitivity", "Specificity", "Accuracy", "TPR", "FPR"])
+    
     # Dataframe to hold the historical data of the stock we are interested in.
-    hist_data = pd.read_csv(stock)
+    hist_data = pd.read_csv("/Users/ks/bot/Pynance/yfinance/stock_data_min/"+stock.upper()+".csv")
     # Name of company
-    company = ((os.path.basename(stock)).split(".csv")[0]) 
+    company = stock
     # Append the closing prices of a stock in prices list
     prices = []
     c = 0
@@ -41,7 +45,7 @@ for stock in list_files:
         if hist_data.iloc[c,4] > float(2.00):
             prices.append(hist_data.iloc[c,4])
         c += 1
-    # prices_df = pd.DataFrame(prices)  # Make a dataframe from the prices list
+
     i = 0
     up_prices=[]
     down_prices=[]
@@ -64,19 +68,19 @@ for stock in list_files:
     avg_loss = []
     #  Loop to calculate the average gain and loss
     while x < len(up_prices):
-        if x < 15:
+        if x < time_period+1:
             avg_gain.append(0)
             avg_loss.append(0)
         else:
             sum_gain = 0
             sum_loss = 0
-            y = x-14
+            y = x-time_period
             while y < x:
                 sum_gain += up_prices[y]
                 sum_loss += down_prices[y]
                 y += 1
-            avg_gain.append(sum_gain/14)
-            avg_loss.append(abs(sum_loss/14))
+            avg_gain.append(sum_gain/time_period)
+            avg_loss.append(abs(sum_loss/time_period))
         x += 1
 
     p = 0
@@ -84,7 +88,7 @@ for stock in list_files:
     RSI = []
     #  Loop to calculate RSI and RS
     while p < len(prices):
-        if p <15:
+        if p <time_period+1:
             RS.append(0)
             RSI.append(0)
         else:
@@ -93,9 +97,10 @@ for stock in list_files:
             RSI.append(100 - (100/(1+RSvalue)))
         p+=1
 
-    RSI_smoothed = talib.RSI(hist_data['Close'], timeperiod=14)
-    
+    #RSI_smoothed calculation
+    RSI_smoothed = talib.RSI(hist_data['Close'], timeperiod=time_period)
 
+    
     #  Creates the csv for each stock's RSI and price movements
     df_dict = {
         'Prices' : prices,
@@ -107,8 +112,9 @@ for stock in list_files:
         'RSI' : RSI,
         'RSI_smoothed' : RSI_smoothed
     }
-    df = pd.DataFrame(df_dict, columns = ['Prices', 'upPrices', 'downPrices', 'AvgGain','AvgLoss', 'RS', "RSI", "RSI_smoothed"])
-    df.to_csv("/Users/ks/bot/Pynance/rsi/rsi_data/"+company+"_RSI.csv", index = False)
+
+    df_rsi = pd.DataFrame(df_dict, columns = ['Prices', 'upPrices', 'downPrices', 'AvgGain','AvgLoss', 'RS', "RSI", "RSI_smoothed"])
+    # df.to_csv("/Users/ks/bot/Pynance/rsi/rsi_data/"+company+"_RSI.csv", index = False)
 
     """
     Code to test the accuracy of the RSI at predicting stock prices
@@ -116,10 +122,9 @@ for stock in list_files:
     -Loop through each time period in a stocks historical data and see if the RSI crossed over the 30% or 70% line.
     -If a crossover did occur, check if the stock’s future price moved as expected.
     -Compute the measurement variables.
-    -Save the accuracy measurements for each stock to the same CSV file for easy comparison.
     """
 
-    Days_Observed = 15
+    Days_Observed = time_period + 1
     Crosses = 0
     nothing = 0
     True_Positive = 0
@@ -129,6 +134,7 @@ for stock in list_files:
     Sensitivity = 0
     Specificity = 0
     Accuracy = 0
+
     while Days_Observed < len(prices)-5:
         if RSI[Days_Observed] <= 30:
             if ((prices[Days_Observed + 1] + prices[Days_Observed + 2] + prices[Days_Observed + 3] + prices[Days_Observed + 4] + prices[Days_Observed + 5])/5) > prices[Days_Observed]:
@@ -166,9 +172,63 @@ for stock in list_files:
     FPR = 1 - Specificity  
 
     # Create a row to add to the compare_stocks
-    add_row = {'Company' : company, 'Days_Observed' : Days_Observed, 'Crosses' : Crosses, 'True_Positive' : True_Positive, 'False_Positive' : False_Positive, 
+    add_row = {'Company' : company, 'Periods_Observed' : Days_Observed, 'Crosses' : Crosses, 'True_Positive' : True_Positive, 'False_Positive' : False_Positive, 
     'True_Negative' : True_Negative, 'False_Negative' : False_Negative, 'Sensitivity' : Sensitivity, 'Specificity' : Specificity, 'Accuracy' : Accuracy, 'TPR' : TPR, 'FPR' : FPR}
 
     # Add the analysis on the stock to the existing Compare_Stocks dataframe
-    compare_stocks = compare_stocks.append(add_row, ignore_index = True) 
-compare_stocks.to_csv("/Users/ks/bot/Pynance/rsi/rsi_power/stock_analysis.csv", index = False)  
+    df_rsi_power = rsi_power.append(add_row, ignore_index = True) 
+    # compare_stocks.to_csv("/Users/ks/bot/Pynance/rsi/rsi_power/stock_analysis.csv", index = False)  
+
+    return df_rsi, df_rsi_power
+
+
+def indicator_bbands(stock, time_period=21):
+
+    """
+    - TODO: time_period is variable (Neural network)
+    - TODO: slope of all bands --> 0 sell signal and vv. 
+    - Use BBands with diff for slope
+    """
+    # Dataframe to hold the historical data of the stock we are interested in.
+    hist_data = pd.read_csv("/Users/ks/bot/Pynance/yfinance/stock_data_min/"+stock.upper()+".csv")
+    # Name of company
+    company = stock
+
+    # Append the closing prices of a stock in prices list
+    prices = []
+    c = 0
+    # Add the closing prices to the prices list and make sure we start at greater than 2 dollars to reduce outlier calculations.
+    while c < len(hist_data):
+        if hist_data.iloc[c,4] > float(2.00):
+            prices.append(hist_data.iloc[c,4])
+        c += 1
+
+    upperband, middleband, lowerband = talib.BBANDS(hist_data['Close'], timeperiod=time_period, nbdevup=2, nbdevdn=2, matype=0)
+    upperband_ema, middleband_ema, lowerband_ema = talib.BBANDS(hist_data['Close'], timeperiod=time_period, nbdevup=2, nbdevdn=2, matype=talib.MA_Type.T3)
+    
+    #  Creates the csv for each stock's bbands
+    df_dict = {
+        'Company' : company,
+        'Prices' : prices,
+        'upperband' : upperband,
+        'lowerband' : lowerband,
+        'middleband' : middleband,
+        'upperband_ema' : upperband_ema,
+        'lowerband_ema' : lowerband_ema,
+        'middleband_ema' : middleband_ema,
+    }
+
+    df_bbands = pd.DataFrame(df_dict, columns = ['Company', 'Prices', 'upperband', 'lowerband', 'middleband','upperband_ema', 'lowerband_ema', "middleband_ema"])
+
+    return df_bbands
+
+def plots():
+    stock = input('Enter Stock symbol: ')
+    time_period = input('Enter desired time period for indicator: ')
+    df_rsi, df_rsi_power = indicator_rsi(stock, time_period)
+    df_bbands = indicator_bbands(stock, time_period)
+
+
+
+if __name__ == '__main__':
+    plots()
